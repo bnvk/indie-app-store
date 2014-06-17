dce8afe14b967cb2fcac55b7633655b411fce978
Bastian Allgeier <mail@bastianallgeier.com>
2014-06-17 17:02:17 +0200

    added noindex metatags

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,6 +4,7 @@
 
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="robots" content="noindex, nofollow" />
 
   <title><?php echo html($site->title() . ' / ' . $page->title()) ?></title>
 
